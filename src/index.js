205cca9ea4ff5a67cb7f412e4c767f961a901834
ipc <israelpadilla@Israels-MacBook-Air.local>
2023-07-29 22:05:53 -0700

    completed code and fixed errors in code to allow npm run start to function

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
+// index.js
+
+// Import statements (if you need any)
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-window.addEventListener("load", function () {
-  console.log("Galactic Super Calculator is loading and js is working");
-});
-
-
-//Buisness Logic//
-class GalacticAgeCalculator {
+// Business Logic (GalacticAgeCalculator class)
+export class GalacticAgeCalculator {
   constructor(age) {
     this.age = age;
     this.planetYears = {
       Mercury: 0.24,
       Venus: 0.62,
+      Mars: 1.88,
       Jupiter: 11.86
     };
   }
@@ -30,5 +29,3 @@ class GalacticAgeCalculator {
     return (futureAge - this.age) / this.planetYears[planet];
   }
 }
-
-ModuleFilenameHelpers.exports = GalacticAgeCalculator;
