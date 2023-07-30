a05df9581a786fd77570cd7345f327790888d174
ipc <israelpadilla@Israels-MacBook-Air.local>
2023-07-29 21:49:42 -0700

    added more code to business logic and fixed errors in code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import Character from "./js/character.js";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 window.addEventListener("load", function () {
-  console.log("Galactic Super Calculator is loading and js is working")
-));
+  console.log("Galactic Super Calculator is loading and js is working");
+});
+
 
 //Buisness Logic//
 class GalacticAgeCalculator {
   constructor(age) {
     this.age = age;
     this.planetYears = {
-      Mercury; 0.24,
+      Mercury: 0.24,
       Venus: 0.62,
       Jupiter: 11.86
     };
@@ -26,4 +26,9 @@ class GalacticAgeCalculator {
     return (this.age - previousAge) / this.planetYears[planet];
   }
 
+  yearsTo(futureAge, planet) {
+    return (futureAge - this.age) / this.planetYears[planet];
+  }
+}
 
+ModuleFilenameHelpers.exports = GalacticAgeCalculator;
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'SuperGalacticeAgeCalculator',
       template: './src/index.html',
       inject: 'body'
     })
